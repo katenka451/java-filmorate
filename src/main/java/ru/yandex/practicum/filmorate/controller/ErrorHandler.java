d40ebc13d853eb19fba838fd87994abd042b2097
katenka451 <pozor451@yandex.ru>
2024-10-09 20:12:44 +0300

    sprint11/dev filmorate (fix Postman check for e-mail & name)

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,6 +20,16 @@ public class ErrorHandler {
     public ErrorResponse handleValidation(final ValidationException e) {
         log.error(e.getMessage());
         return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValid(final MethodArgumentNotValidException e) {
+        String errorText = "Некорректно задано значение поля " +
+                e.getBindingResult().getFieldErrors().getFirst().getField();
+        log.error(e.getMessage());
+        log.error(errorText);
+        return new ErrorResponse(errorText);
     }
 
     @ExceptionHandler
