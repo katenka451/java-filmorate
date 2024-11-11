f0c0ce1a3bb576a80f92586877779b5c8f9d56ce
katenka451 <pozor451@yandex.ru>
2024-11-11 16:37:11 +0300

    sprint12/dev filmorate (codestyle fix v2)

--- src/test/java/ru/yandex/practicum/filmorate/dal/FilmRepositoryTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dal/FilmRepositoryTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.annotation.DirtiesContext;
 import ru.yandex.practicum.filmorate.dal.mappers.FilmRowMapper;
 import ru.yandex.practicum.filmorate.model.Film;
 
